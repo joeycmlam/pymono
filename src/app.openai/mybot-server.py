e8a2311fd185d96298e3c16a61f6bde9403e6bf2
joeylam <joey.cm.lam@gmail.com>
2023-12-26 13:25:53 +0800

    WIP

--- src/app.openai/mybot-server.py
+++ src/app.openai/mybot-server.py
@@ -1,27 +1,5 @@
-# This is a simplified example of a chatbot script using Python
-# It requires further development and integration with a platform
 
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-# Create a new chatbot instance
-chatbot = ChatBot('CustomerServiceBot')
-
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Train the chatbot with your custom dataset
-trainer.train("data/morningstart-1.html")
-
-# Function to get a response from the chatbot
-def get_bot_response(user_input):
-    bot_response = chatbot.get_response(user_input)
-    return bot_response
-
-# Example interaction loop with the chatbot
-while True:
-    try:
-        user_input = input("You: ")
-        print("Bot: ", get_bot_response(user_input))
-    except(KeyboardInterrupt, EOFError, SystemExit):
-        break
+{
+    cat: "mic",
+    date_of_birth: "2019-01-01",
+}
